03134749daff9b79586f7d358f0f68c8b352f820
masato nishio <var.masatonishio@gmail.com>
2024-01-31 09:57:06 +0900

    fix(front):タイトルと導入テキストを修正した。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'mst-nsh.github.io[my page]';
+  title = 'mst-nsh';
 }
