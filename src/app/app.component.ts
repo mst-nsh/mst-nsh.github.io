1f25ddf3653908101ba710c8fddfe40bcd96142c
masato nishio <var.masatonishio@gmail.com>
2025-04-19 11:54:58 +0900

    ng update @18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from '@src/app/footer/footer.component';
 import { HeaderComponent } from '@src/app/header/header.component';
@@ -8,13 +7,7 @@ import { HeaderComponent } from '@src/app/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    MatToolbarModule,
-    FooterComponent,
-    HeaderComponent,
-  ],
+  imports: [CommonModule, MatToolbarModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
