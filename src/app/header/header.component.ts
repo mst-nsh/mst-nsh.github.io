746aa30aa2add9eb29359a999133d5300c36eda0
mst-nishio <67684762+mst-nsh@users.noreply.github.com>
2025-05-15 20:12:43 +0900

    ダークモードを実装した。 (#36)
    * styles.scssにファイルを変更した
    
    * prebuilt-themes を緑色に変更した。
    
    * トグルを追加した
    
    * フッター、ヘッダーのカラーを変更した
    
    * ダークモードのトグルを実装した。
    
    * dummy-spaceを削除した。
    
    * フッターのレイアウトを変更した
    
    * ダークモード時にborderを追加した。
    
    * ダークモード時にフッターにアイコンを追加した

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { ThemeService } from './../theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatIconModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   title = 'mst-nsh.github.io';
+
+  isDarkTheme$!: Observable<boolean>;
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.isDarkTheme$ = this.themeService.isDarkTheme$;
+  }
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
