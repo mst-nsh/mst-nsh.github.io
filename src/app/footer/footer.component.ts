2a54bc4b7f51b92e40648a76739a9d378667c5df
mst-nishio <67684762+mst-nsh@users.noreply.github.com>
2025-05-15 20:13:45 +0900

    dev2main_20250515_01 (#37)
    * 技術スタックを更新した。
    
    * Angular Materialをv19に更新した。 (#35)
    
    * ng update @angular/material@18
    
    * ng update @angular/material@19
    
    * ダークモードを実装した。 (#36)
    
    * styles.scssにファイルを変更した
    
    * prebuilt-themes を緑色に変更した。
    
    * トグルを追加した
    
    * フッター、ヘッダーのカラーを変更した
    
    * ダークモードのトグルを実装した。
    
    * dummy-spaceを削除した。
    
    * フッターのレイアウトを変更した
    
    * ダークモード時にborderを追加した。
    
    * ダークモード時にフッターにアイコンを追加した

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { ThemeService } from './../theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isDarkTheme$!: Observable<boolean>;
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.isDarkTheme$ = this.themeService.isDarkTheme$;
+  }
+}
