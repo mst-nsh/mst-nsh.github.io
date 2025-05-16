a66f8094fad001d757446c0be9c4cb7643fbe58b
masato nishio <var.masatonishio@gmail.com>
2025-05-16 19:24:56 +0900

    フッターにgithub,zennのリンクを追加した。

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from './../theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isDarkTheme$!: Observable<boolean>;
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.isDarkTheme$ = this.themeService.isDarkTheme$;
+  }
+
+  openZenn() {
+    window.open('https://zenn.dev/mst_nishio');
+  }
+  openGithub() {
+    window.open('https://github.com/mst-nsh');
+  }
+}
